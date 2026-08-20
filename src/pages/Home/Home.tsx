7e3c2f13053cb516e32729673a23977d6a0053c3
Alyssa van Gorkom <alyssavangorkom@Alyssas-MacBook-Pro-2.local>
2026-08-20 12:06:03 -0400

    feat: add break

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,16 @@
-import { Avatar, Button } from "@mui/material";
+import { Avatar, Button, Card, CardContent, Typography } from "@mui/material";
 import headshot from "../../images/headshot.jpg";
+import Break from "../../components/Break/Break";
 
 export const section =
-  "flex flex-col justify-start items-center gap-3 w-full lg:w-4/5 mx-auto";
+  "flex flex-col justify-start items-center gap-8 w-full lg:w-4/5 mx-auto";
 
 const Home = () => {
   return (
     <div className={section}>
       <div className="flex flex-row gap-8 w-full">
         <Avatar
-          className="!w-1/2 !h-auto"
+          className="!w-1/2 !max-w-[500px] !h-auto"
           alt="Alyssa Tripp Photo"
           src={headshot}
         />
@@ -35,6 +36,64 @@ const Home = () => {
             Contact Me!
           </Button>
         </div>
+      </div>
+
+      <Break />
+
+      <div className="flex flex-row gap-8 w-full">
+        <Card className="!bg-fern !rounded-lg flex-1">
+          <CardContent>
+            <Typography
+              variant="h4"
+              className="font-bold !mb-4 text-apricot-cream"
+            >
+              Modern Stack
+            </Typography>
+            <Typography variant="body1" className="mb-2 text-apricot-cream">
+              Proficient in React, Typescript, and JavaScript for building
+              modern web applications.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card className="!bg-fern !rounded-lg flex-1">
+          <CardContent>
+            <Typography
+              variant="h4"
+              className="font-bold !mb-4 text-apricot-cream"
+            >
+              Real World Experience
+            </Typography>
+            <Typography variant="body1" className="mb-2 text-apricot-cream">
+              2 years of internship experience - delivering real products to
+              real sponsors.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card className="!bg-fern !rounded-lg flex-1">
+          <CardContent>
+            <Typography
+              variant="h4"
+              className="font-bold !mb-4 text-apricot-cream"
+            >
+              Personality Hire
+            </Typography>
+            <Typography variant="body1" className="mb-2 text-apricot-cream">
+              Fun and enthusiastic! I love teamwork and working with people!
+              (yes, that includes pair programming)
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+
+      <Break />
+
+      <div className="flex flex-col justify-start w-full">
+        <Typography
+          variant="h3"
+          className="text-rosewood !font-black text-start "
+        >
+          About Me
+        </Typography>
       </div>
     </div>
   );
