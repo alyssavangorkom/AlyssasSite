d1bab7ff24d759574f20c9c02e86f1b22021439d
Alyssa van Gorkom <alyssavangorkom@Alyssas-MacBook-Pro-2.local>
2026-08-20 10:36:30 -0400

    feat: nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AppBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App flex flex-col h-screen">
+      <AppBar />
+      <header className="App-header text-midnight-violet bg-apricot-cream flex flex-1">
+        <p>Under Construction!!!</p>
       </header>
     </div>
   );
